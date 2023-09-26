25ab228ab6441ff21a63688ca282c45a4dd14362
beeyah2002 <beesui2545@email.com>
2023-09-26 09:24:41 +0700

    update

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
+const path = require("path")
 
 const base_url = "http://localhost:3000";
 
